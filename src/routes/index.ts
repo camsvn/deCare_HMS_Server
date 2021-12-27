4c286ce67d8793f8bf26f334ab87961ee910aaec
Amal Salvin Joseph <amalsalvin97@gmail.com>
2021-12-27 17:52:58 +0530

    added tomogram route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router, Request, Response } from "express";
 
 import authRouter from '../controllers/auth'
+import tomogramRouter from '../controllers/tomogram'
 import indexRouter from '../controllers'
 
 const router = Router();
 
-router.use('/login', authRouter);
+router.use('/auth', authRouter);
+router.use('/tomogram', tomogramRouter);
 
 router.use('/', indexRouter);
 
