f195670b0d1c0a82470969a8d358a0631dd49219
Amal Salvin Joseph <amalsalvin97@gmail.com>
2022-01-04 08:19:18 +0530

    code refactor: clean code

--- src/controllers/tomogram/index.ts
+++ src/controllers/tomogram/index.ts
@@ -1,39 +1,9 @@
 import { Router} from "express";
-import {getloginController, loginController, uploadTomogramController} from './tomogram';
+import { uploadTomogramController } from './tomogram';
 import { uploadFile } from "../../middlewares/multer";
-
-// import multer from 'multer';
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//       cb(null, './uploads/')
-//   },
-  
-//   filename: function (req: any, file: any, cb: any) {
-//       cb(null, file.originalname)
-//   }
-// });
-
-// const fileFilter = (req: any,file: any,cb: any) => {
-//   if (file.mimetype === "image/jpg"  || 
-//      file.mimetype ==="image/jpeg"  || 
-//      file.mimetype ===  "image/png") {   
-//     cb(null, true);
-//  } else {
-//     cb(new Error("Image uploaded is not of type jpg/jpeg or png"),false);
-//   }
-// }
-
-// const upload = multer({storage: storage, fileFilter : fileFilter});
-
-// var upload = multer({ dest: 'uploads/' }); //setting the default folder for multer
-
 
 const router = Router();
 
-router.post('/login', loginController);
-router.get('/login', getloginController);
-// router.post('/', upload.array('images', 2), uploadTomogramController);
 router.post('/', uploadFile, uploadTomogramController);
 
 export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,13 +3,17 @@ import { Router, Request, Response } from "express";
 import authRouter from '../controllers/auth'
 import tomogramRouter from '../controllers/tomogram'
 import indexRouter from '../controllers'
+import { successResponse } from "../helpers/JSend";
 
 const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/tomogram', tomogramRouter);
+router.use('/test', indexRouter);
+router.get('/', (req, res) => {
+  res.json(successResponse('Route /api works'))
+})
 
-router.use('/', indexRouter);
 
 
 export default router; 
