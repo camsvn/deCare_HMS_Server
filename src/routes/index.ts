a128c1706accc4172f7c2186b50128c2008869bf
Amal Salvin Joseph <amalsalvin97@gmail.com>
2022-01-14 15:55:50 +0530

    set base api route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -11,6 +11,7 @@ router.use('/auth', authRouter);
 router.use('/tomogram', tomogramRouter);
 router.use('/test', indexRouter);
 router.get('/', (req, res) => {
+  console.log(req.connection.remoteAddress)
   res.json(successResponse('Route /api works'))
 })
 
